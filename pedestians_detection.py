af7f232d2d21980a3c9d0a07e59651d542785cea
poojakish <64463721+poojakish@users.noreply.github.com>
2020-07-02 19:40:48 +0530

    Add files via upload

---
+++ pedestians_detection.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 18:56:32 2020
+
+@author: samsung
+"""
+
+import cv2
+
+body_cascade= cv2.CascadeClassifier("haarcascade_fullbody.xml")
+
+cap=cv2.VideoCapture("pedestrians.mp4") # add your video here 
+
+while True:
+    ret,frame=cap.read()
+    #detecting the pedestrians 
+    body_rect=body_cascade.detectMultiScale(frame,1.3,5)
+        
+    for x,y,w,h in body_rect:
+        frame= cv2.rectangle(frame,(x,y),(x+w,y+h),(255,25,0),4)
+            
+    cv2.imshow('pedestians',frame)
+    
+    if cv2.waitKey(1)==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
